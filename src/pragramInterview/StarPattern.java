400f41ec4dc897850a1cf8e9584d6bb02e9b7fb7
DineshQAAugust <sara1006554@gmail.com>
2022-08-10 20:31:41 +0530

    V1.0

---
+++ src/pragramInterview/StarPattern.java
@@ -0,0 +1,32 @@
+package pragramInterview;
+
+public class StarPattern {
+
+	public static void main(String[] args) {
+
+		// int[] a={8,6,7,4,5,3,1,2,9};
+
+		int[] a = { 8, 6, 7, 4, 5, 3, 1, 2, 9 };
+		int tmp;
+
+		for (int i = 0; i < a.length; i++) {
+
+			for (int j = i + 1; j < a.length; j++) {
+				if (a[i] > a[j]) {
+					tmp = a[i];
+					a[i] = a[j];
+					a[j] = tmp;
+
+				}
+
+			}
+
+		}
+
+		for (int k : a) {
+			System.out.println(k);
+		}
+
+	}
+
+}
